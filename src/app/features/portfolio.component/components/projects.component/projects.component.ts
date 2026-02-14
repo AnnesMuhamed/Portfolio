65cc7896fd5b907ad63b69f307ecaa183c7b618a
Annes Muhamed <aernnesto89@gmail.com>
2026-02-14 20:36:24 +0100

    Created Projects Content

--- src/app/features/portfolio.component/components/projects.component/projects.component.ts
+++ src/app/features/portfolio.component/components/projects.component/projects.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
-export class ProjectsComponent {
 
+export class ProjectsComponent {
+  activeProjectId = 'Da Bubble';
+
+  projects = [
+    { id: 'Da Bubble', title: 'Da Bubble' },
+    { id: 'El Pollo Loco', title: 'El Pollo Loco' },
+    { id: 'Join', title: 'Join' },
+    { id: 'Ongoing Project', title: 'Ongoing Project' },
+  ];
+
+  setActiveProject(id: string) {
+    this.activeProjectId = id;
+  }
 }
+
