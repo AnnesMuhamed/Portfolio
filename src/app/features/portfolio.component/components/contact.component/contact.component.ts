46a9edf92592971d9fbbbb3a24d3de360e1af7ac
Annes Muhamed <aernnesto89@gmail.com>
2026-02-18 16:27:58 +0100

    Created Contact me section I have created the contact section and added a heading, descriptive text, and contact address (email and phone number).

--- src/app/features/portfolio.component/components/contact.component/contact.component.ts
+++ src/app/features/portfolio.component/components/contact.component/contact.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-contact.component',
-  imports: [],
+  selector: 'app-contact',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
--- src/app/features/portfolio.component/portfolio.component.ts
+++ src/app/features/portfolio.component/portfolio.component.ts
@@ -5,13 +5,14 @@ import { WhyMeComponent } from './components/why-me.component/why-me.component';
 import { ProjectsComponent } from './components/projects.component/projects.component';
 import { SkillComponent } from "./components/skill.component/skill.component";
 import { ReferencesComponent } from "./components/references/references"; 
+import { ContactComponent } from "./components/contact.component/contact.component";
 
 
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [HeroComponent, Header, WhyMeComponent, ProjectsComponent, SkillComponent, ReferencesComponent],
+  imports: [HeroComponent, Header, WhyMeComponent, ProjectsComponent, SkillComponent, ReferencesComponent, ContactComponent],
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss'],
 })
